695c841e19baae31e60b783a37cae6b882903c81
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2017-08-01 17:04:52 +0200

    Store results

---
+++ S15/tests/dbbench/queries/out/ccqserv100/README.rst
@@ -0,0 +1,31 @@
+Platform
+========
+
+ccqserv100-124
+
+Operating system
+================
+
+CentOS 7.3
+
+Node
+====
+
+16 cores, 16 GB  per node
+25 worker nodes enabled
+
+Storage
+=======
+
+Local storage for each node
+
+Misc
+====
+
+- 2nd join query is slow
+
+Orchestrator
+============
+
+Kubernetes 1.5.2
+Docker 1.12.3 (using LimitMEMLOCK ~10GB)
--- S15/tests/dbbench/run-all.sh
+++ S15/tests/dbbench/run-all.sh
@@ -3,12 +3,14 @@
 set -x
 set -e
 
-FILES=$(ls queries/*.ini)
+FILES=$(ls queries/1_*.ini)
+OUTDIR="queries/out"
 for f in $FILES 
 do
     echo "Processing $f file..."
     outfile=$(basename "$f")
     outfile=$(echo "$outfile" | sed 's/\.ini$/\.stats/')
-    dbbench --database=LSST --query-stats-file="queries/out/${outfile}" --username=qsmaster --host=ccqserv100 --port=4040 "$f" 
+    mkdir -p "$OUTDIR"
+    dbbench --database=LSST --query-stats-file="$OUTDIR/${outfile}" --username=qsmaster --host=ccqserv100 --port=4040 "$f" 
 done
 
